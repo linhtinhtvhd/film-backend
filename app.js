a0cdbe275281cf22d270005404a77267a3d857f1
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-28 10:45:55 +0700

    fid

--- app.js
+++ app.js
@@ -28,18 +28,11 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }));
-app.use(cors({origin:"*"}))
+app.use(cors({origin:"*",
+methods:["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+credentials:true}))
 app.use(morgan("dev"));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.header('Origin'));
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(express.json({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(passport.initialize());
