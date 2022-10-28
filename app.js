159bcb8ddcd24340924bc5fcb25a84a3029a7808
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-28 20:05:50 +0700

    ds

--- app.js
+++ app.js
@@ -21,19 +21,18 @@ const __dirname = path.dirname(__filename);
 const port = process.env.PORT;
 const app = express();
 
-app.use(cors({origin:"*",
-credentials:true}))
+app.use(cors())
 
-// app.use(function(req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept","X-Auth-Token","Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept","X-Auth-Token","Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(session({
   secret: 'somethingsecretgoeshere',
