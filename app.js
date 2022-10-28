f6caa6cb789a6f5b9b32be22af74b66ea2b505b7
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-28 20:41:19 +0700

    sd

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors())
 
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.setHeader("Access-Control-Allow-Origin", "https://film-linhtinhtvhd.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
