dc5f7944b9a2cdc771534936056c26ae552d966d
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-28 14:52:20 +0700

    d

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5000
 app.use(express.json({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(passport.initialize());
 app.use(passport.session());
-app.use("/auth", AuthRouter);
+app.use("/auth", AuthRouter,function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*")})
 app.use(cookieParser());
 app.get("/home",(req,res,next)=>{
   res.json(req.user)
