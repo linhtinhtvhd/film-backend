97e919f218c79db480ee348d40f2a42a43269d08
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-28 16:40:59 +0700

    f

--- app.js
+++ app.js
@@ -31,17 +31,17 @@ app.use(session({
 app.use(morgan("dev"));
 const whiteList = ['http://localhost:3000','https://film-linhtinhtvhd.vercel.app']
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-app.use(cors())
+// app.use(function(req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 
 
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
