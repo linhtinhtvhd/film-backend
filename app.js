62f407ee9b64ad39094f9b0667a3f347adb4083b
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-28 16:09:37 +0700

    sd

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(morgan("dev"));
 app.use(cors())
 
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
