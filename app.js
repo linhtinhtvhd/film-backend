7fde9a050bf23baf77713e11823c31d301407047
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-28 14:48:45 +0700

    cvcv

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(morgan("dev"));
 app.use(cors())
 app.options('*', cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  res.header("Access-Control-Allow-Origin", "https://film-pinklink.herokuapp.com");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
