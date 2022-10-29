952d81fc2a7db3ab7431b96ebbf70a4e7f4d9513
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-29 15:30:25 +0700

    dsa

--- app.js
+++ app.js
@@ -21,25 +21,6 @@ const __dirname = path.dirname(__filename);
 const port = process.env.PORT;
 const app = express();
 
-app.use(cors())
-
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept","X-Auth-Token","Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-// app.all ('/', function (req, res, next) {
-//   res.header ("Access-Control-Allow-Origin", "*");
-//   res.header ("Access-Control-Allow-Headers", "X-Request-With");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next()
-// });
 
 app.use(session({
   secret: 'somethingsecretgoeshere',
@@ -47,11 +28,18 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+app.use(cors())
 app.use(morgan("dev"));
-
-
-
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(express.json({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(passport.initialize());
