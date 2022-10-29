1b8683f488e3d5ff6cf78adc3d4a2d3139f805b8
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-29 15:20:48 +0700

    asd

--- facebook.js
+++ facebook.js
@@ -9,7 +9,7 @@ const clienSecret ="190759f47ea9be50fe5fade5d8790da4"
 passport.use(new Strategy({
     clientID: clienId,
     clientSecret: clienSecret,
-    callbackURL: "/auth/facebook/callback",
+    callbackURL: "http://localhost:3001/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'photos', 'email']
   },
   function(accessToken, refreshToken, profile, done) {
