d952b04177546c7a34be1a28904889bb5baabca3
linhtinhthhd <linhtinhthhd@gmail.com>
2022-11-03 20:08:54 +0700

    fix

--- router/auth.js
+++ router/auth.js
@@ -3,7 +3,7 @@ const AuthRouter = express.Router();
 import passport from "passport";
 import '../passport/facebook'
 
-const CLIENT_URL = "http://localhost:3000";
+const CLIENT_URL = "https://film-linhtinhtvhd.vercel.app";
 
 AuthRouter.get("/login/success", (req, res) => {
   
